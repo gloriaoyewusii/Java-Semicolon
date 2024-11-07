d0b58a8095cc85d432991f178b9ca7715c7dc7e2
gloryacodes <gloriaoyewusii@gmail.com>
2024-11-07 08:33:48 +0100

    attempt

---
+++ Java/Jennifer/Assignment/MBTIAssignment/PersonalityTest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PersonalityTest{
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("What is your name: ");
+	String name = input.nextLine();
+	String answer = input.nextLine();
+	String[][] questions = {
+	{"A. expend energy, enjoy groups", "B. conserve energy, enjoy one-on-one"},
+	{"A. Interpret literally", "look for meaning and possibilities"
+
+	};
+	String options[] = new String[20];
+	
+	
+	
+	}
+}
+
+
+
+
+//for (int i = 0; i < questions.length; i++){
+		//System.out.print("At a party do you: a. Interact with many //including strangers b. Interact with a few known to you");
+		//for(int j = 0; j < options.length; j++){
+		//System.out.print("Answer: ");
+		
+		//}
+	//}	System.out.println();
